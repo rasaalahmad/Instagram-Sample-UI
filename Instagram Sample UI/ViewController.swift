25843b47a93c415f8902f05398801c9faf636c44
Rasaal Ahmad <56187211+rasaalahmad@users.noreply.github.com>
2023-05-09 16:24:51 -0700

    Initial Commit

---
+++ Instagram Sample UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram Sample UI
+//
+//  Created by Rasaal Ahmad on 09/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
